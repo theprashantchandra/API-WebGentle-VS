357373ce926ad713d2955d896553d70c89beff98
theprashantchandra <97657867+theprashantchandra@users.noreply.github.com>
2022-07-26 08:05:25 +0530

    #40

---
+++ ConsoleApp1/CustomMiddleware1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    public class CustomMiddleware1 : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.Write.Async("Hello from new file 1 \n");
+            await next(context);
+            await context.Response.Write.Async("Hello from new file 1 \n");
+        }
+    }
+}
